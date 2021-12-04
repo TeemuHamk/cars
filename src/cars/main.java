7667a2767b4ec92d0add559e1ba8ca9a14fa5776
TeemuHamk <teemu21017@student.hamk.fi>
2021-12-04 21:39:38 +0200

    folder structure

---
+++ src/cars/main.java
@@ -0,0 +1,104 @@
+package cars;
+
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Auto auto1 = new Auto();
+		auto1.malli = "Mersu";
+		auto1.merkki = "Vayrynen";
+		auto1.Bensanmaara = 10;
+		auto1.tankkaus = 5;
+		auto1.NaytaTiedot();
+		
+
+// 		auto1.kiihdyta();
+//		auto1.tankkaa(auto1.Bensanmaara);
+
+		Auto auto2 = new Auto("Mercedes", "C-Class", 10);
+		auto2.NaytaTiedot();
+		
+	}
+
+} // main class ends
+
+class Auto
+{ // auto class starts
+	
+	// Attribuutit
+	
+	String merkki;
+	String malli;
+	int Bensanmaara;
+	int tankkaus;
+	
+	// Muodostimet
+	
+	public Auto()
+	{
+	merkki="";
+	malli="";
+	Bensanmaara=0;
+	tankkaus=0;
+	}
+	
+	public Auto(String make, String model, int gas)
+	{
+		
+		merkki = make;
+		malli = model;
+		Bensanmaara = gas;
+	}
+	
+	// Metodit
+	
+	
+	public void jarruta()
+	{ // Jarruta alkaa
+		
+		System.out.print("Auto jarruttaa");
+		
+	} // Jarruta loppuu
+	
+	public void kiihdyta()
+	{ // kiihdyta alkaa
+		
+		if (Bensanmaara == 0)
+		{
+			System.out.println("Tankissa ei ole bensaa");
+		}
+		else
+		{
+		System.out.println("Auto kiihdyttaa");
+		Bensanmaara = -1;
+		}
+		
+	} // kiihdyta loppuu
+	
+	public void NaytaTiedot()
+	{ // naytatiedot alkaa
+		
+		System.out.println("Auton merkki: " + merkki);
+		System.out.println("Auton malli : " + malli);
+		System.out.println("Tankissa bensaa : " + Bensanmaara);
+		
+	} // naytatiedot loppuu
+	
+	public void tankkaa(int i1)
+	{
+
+		System.out.println("Tankissa on bensaa :" + i1);
+		System.out.println("Tankkaus : " + tankkaus);
+		i1 = i1 + tankkaus;
+		System.out.println("Tankissa bensaa tankkauksen jälkeen: " + i1);
+		
+		 
+		
+		
+		
+	}
+	
+	
+} // Auto class ends
